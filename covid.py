148e4aa08e4a11592162cdfbae39d6fe5ee75711
Shad Sharma <shadanan@gmail.com>
2020-10-04 15:53:32 -0700

    Add ability to show country flags. (#9)
    * Add ability to show country flags.
    
    * Address @niwako's comments.

--- covid.py
+++ covid.py
@@ -5,27 +5,53 @@ import streamlit as st
 import data
 
 
+def flag_header(iso_3166, header):
+    """Streamlit function to display some header text along with a country flag.
+
+    Parameters
+    ----------
+    iso_3166 : str
+        The ISO 3166 two letter country code for the desired flag.
+
+    header : str
+        The header text to display next to the flag.
+    """
+    flag_url = data.flag_url(iso_3166)
+    st.markdown(
+        f'<h2><img src="{flag_url}" height="32" /> {header}</h2>',
+        unsafe_allow_html=True,
+    )
+
+
 cdf = data.covid()
-pdf = data.population()
 wdf = data.covid_by_country()
 
 """
 # Covid Data
 """
 
-latest = max(cdf.last_update)
-ldf = cdf[(cdf.last_update == latest)]
+ldf = wdf[(wdf.file_date == max(wdf.file_date))]
 
 confirmed = ldf.confirmed.sum()
 recovered = ldf.recovered.sum()
 deaths = ldf.deaths.sum()
 
 f"""
-Last updated {humanize.naturaltime(latest)}.
+Last updated {humanize.naturaltime(max(cdf.last_update))}.
 
 ## Worldwide
 
 | Total cases   | Recovered     | Deaths     |
 | ---           | ---           | ---        |
 | {confirmed:,} | {recovered:,} | {deaths:,} |
+"""
+
+# Get the latest country level data for the United States.
+row = ldf[ldf.country_region == "United States"].iloc[0]
+flag_header(row.iso_3166, row.country_region)
+
+f"""
+| Total cases       | Recovered         | Deaths         |
+| ---               | ---               | ---            |
+| {row.confirmed:,} | {row.recovered:,} | {row.deaths:,} |
 """
